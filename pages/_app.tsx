08a433feccba7c067e8a9937a4321facc69ddc53
Andre Christoga Pramaditya <caxvis@gmail.com>
2024-08-09 00:40:43 +0700

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-NEZ5NLCB5P" />
     </SessionProvider>
   );
 };
